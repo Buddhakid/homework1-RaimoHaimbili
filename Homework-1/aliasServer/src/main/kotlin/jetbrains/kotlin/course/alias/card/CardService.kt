696a5f22efb7f92ae2d2c460e286f37c15593395
Buddhakid <96297559+Buddhakid@users.noreply.github.com>
2025-04-01 17:47:19 +0200

    Update CardService.kt

--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/card/CardService.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/card/CardService.kt
@@ -1,12 +1,37 @@
 package jetbrains.kotlin.course.alias.card
 
+import jetbrains.kotlin.course.alias.util.IdentifierFactory
+import jetbrains.kotlin.course.alias.util.words
 import org.springframework.stereotype.Service
 
 @Service
-class CardService {
-    private fun generateCards(): List<Card> = TODO("Not implemented yet")
 
-    private fun List<String>.toWords(): List<Word> = TODO("Not implemented yet")
+class CardService(
 
-    fun getCardByIndex(index: Int): Card = TODO("Not implemented yet")
+    private val identifierFactory: IdentifierFactory = IdentifierFactory()
+) {
+   private val cards: List<Card> = generateCards()
+
+    companion object {
+        private const val WORDS_IN_CARD = 4
+        val cardsAmount = words.size / WORDS_IN_CARD
+    }
+
+    private fun List<String>.toWords(): List<Word> = this.map { Word(it) }
+
+    private fun generateCards(): List<Card> {
+        return words.shuffled()
+            .chunked(WORDS_IN_CARD)
+            .take(cardsAmount)
+            .map { wordsChunk ->
+                Card(
+                    id = identifierFactory.uniqueIdentifier(),
+                    words = wordsChunk.toWords()
+                )
+            }
+    }
+
+    fun getCardByIndex(index: Int): Card {
+        return cards[index]
+    }
 }
